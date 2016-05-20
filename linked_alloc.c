b29b6c5e6b97e0002780bb68c4a80b3a1b1e2e7f
AxelTLarsson <mail@axellarsson.nu>
2016-05-20 13:15:44 +0200

    Formatted linked

--- linked_alloc.c
+++ linked_alloc.c
@@ -8,9 +8,9 @@
 
 typedef struct list_t list_t;
 struct list_t {
-				size_t size;												 /* size including list_t */
-				int in_use;												 /* if the block is used or not */
-				list_t* next;												 /* next available block. */
+	size_t size;												 /* size including list_t */
+	int in_use;												 /* if the block is used or not */
+	list_t* next;												 /* next available block. */
 };
 
 list_t* allocate_space(list_t*, size_t);
@@ -19,63 +19,63 @@ static list_t* base = NULL;
 
 void *malloc(size_t size)
 {
-				if (size <= 0) {
-								return NULL;
-				}
+	if (size <= 0) {
+		return NULL;
+	}
 
-				size_t real_size = size + sizeof(list_t);
+	size_t real_size = size + sizeof(list_t);
 
-				list_t* last = base;
-				list_t* r = find_block(&last, size);
+	list_t* last = base;
+	list_t* r = find_block(&last, size);
 
-				if (r == NULL) {
-								r = allocate_space(last, size);
-								if (r == NULL)
-												return NULL;
+	if (r == NULL) {
+		r = allocate_space(last, size);
+		if (r == NULL)
+			return NULL;
 
-								if (base == NULL)
-												base = r;
-				}
+		if (base == NULL)
+			base = r;
+	}
 
-				return (r + 1);
+	return (r + 1);
 
 }
 
 list_t* allocate_space(list_t* last, size_t size) {
 
-				list_t* meta = sbrk(0);
-				void* data = sbrk(size + sizeof(list_t));
-				if (data == (void*) -1) {
-								return NULL;
-				}
+	list_t* meta = sbrk(0);
+	void* data = sbrk(size + sizeof(list_t));
+	if (data == (void*) - 1) {
+		return NULL;
+	}
 
-				meta->size = size;
-				meta->next = NULL;
-				meta->in_use = 1;
-				if (last != NULL) {
-								last->next = data;
-				}
+	meta->size = size;
+	meta->next = NULL;
+	meta->in_use = 1;
+	if (last != NULL) {
+		last->next = data;
+	}
 
-				return meta;
+	return meta;
 }
 
 list_t* find_block(list_t** last, size_t size) {
 
-				list_t* current = base;
-				while (current && (current->in_use || !(current->size >= size))) {
+	list_t* current = base;
+	while (current && (current->in_use || !(current->size >= size))) {
 
-								*last = current;
-								current = current->next;
+		*last = current;
+		current = current->next;
 
-				}
+	}
 
-				return current;
+	return current;
 
 }
 
 
 
 void free(void *ptr) {
-				list_t* block = ((list_t*) ptr) - 1;
-				block->in_use = 0;
+	list_t* block = ((list_t*) ptr) - 1;
+	block->in_use = 0;
 }
