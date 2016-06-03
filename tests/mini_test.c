bed0c49c5f664732eab3e32b0e66f3ce7cd0bb2f
AxelTLarsson <mail@axellarsson.nu>
2016-06-03 12:36:07 +0200

    Add useful x*alloc in mini_test.c

--- tests/mini_test.c
+++ tests/mini_test.c
@@ -4,39 +4,69 @@
 #include <unistd.h>
 #include <sys/types.h>
 
+/*
+Check some simple stuff in malloc: not-null-pointer, etc
+*/
+static void* xmalloc(size_t size)
+{
+	void* p = malloc(size);
+	assert(p != NULL);
+	return p;
+}
+
+static void* xcalloc(size_t elems, size_t size)
+{
+	void* p = calloc(elems, size);
+	assert(p != NULL);
+	return p;
+}
+
+static void* xrealloc(void* p0, size_t size)
+{
+	void* p = realloc(void* p0, size);
+	assert(p != NULL);
+	return p;
+}
+
 int main()
 {
 
 	char *str;
 
-	str = (char *) malloc(15);
+	str = (char *) xmalloc(15);
+	assert(str != NULL);
 	strcpy(str, "tutorialspoin1");
 	printf("String = %s,  Address = %p\n", str, (void*)str);
 	free(str);
 
-	str = (char *) malloc(15);
+	str = (char *) xmalloc(15);
+	assert(str != NULL);
 	printf("Address = %p\n", (void*)str);
 	strcpy(str, "tutorialspoin2");
-	str = (char *) malloc(6400);
+	str = (char *) xmalloc(6400);
+	assert(str != NULL);
 	printf("Address = %p\n", (void*)str);
 	free(str);
 
-	str = (char *) malloc(6400);
+	str = (char *) xmalloc(6400);
+	assert(str != NULL);
 	printf("Address = %p\n", (void*)str);
 	strcpy(str, "tutorialspoin2");
 	printf("String = %s,  Address = %p\n", str, (void*)str);
 
-	char *str2 = (char *) malloc(15);
+	char *str2 = (char *) xmalloc(15);
+	assert(str != NULL);
 	printf("Address = %p\n", str);
 	strcpy(str2, "tutorialspoin2");
 	printf("String = %s,  Address = %p\n", str2, (void*)str2);
 
-	str = (char *) realloc(str, 15);
+	str = (char *) xrealloc(str, 15);
+	assert(str != NULL);
 	printf("Address = %p\n", str);
 	strcpy(str, "tutorialspoint");
 	printf("String = %s,  Address = %p\n", str, (void*)str);
 
-	str = (char *) calloc(1, 15);
+	str = (char *) xcalloc(1, 15);
 	printf("Address = %p\n", str);
 	strcpy(str, "tutorialspoint");
 	printf("String = %s,  Address = %p\n", str, (void*)str);
